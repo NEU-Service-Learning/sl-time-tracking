6824047818b54d8d28c0d4d83acae872206c5922
nshenkman <nikita.shenkman@gmail.com>
2016-10-18 12:26:00 -0400

    setup component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Wrapper from './components/Wrapper'
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
 			<Router history={browserHistory}>
 		    <Route path="/" component={Wrapper}>
 					<IndexRoute component={Home} />
+					<Route path="/login" component={Login} />
 		    </Route>
 		  </Router>
     );
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Login extends Component {
+  render () {
+    return (
+      <div className="ui container">
+       login
+      </div>
+    )
+  }
+}
+
+export default Login
