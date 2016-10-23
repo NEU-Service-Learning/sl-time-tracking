903f8dc7e806f6cce63a35edaab092aeb824be38
nshenkman <nikita.shenkman@gmail.com>
2016-10-23 17:55:13 -0400

    simple login page but isnt connected to anything

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import Button from '../components/Button'
 
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: "", password: ""};
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert("Email is: " + this.state.email);
+  }
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value, password : this.state.password})
+  }
+  handleChangePassword(event) {
+    this.setState({password: event.target.value, email : this.state.email})
+  }
   render () {
     return (
       <div className="ui container">
-       login
+        <div className="ui column stackable centered grid">
+
+        <form className="ui six wide column form">
+          <div className="field">
+                <label>Email</label>
+                <input type="text" name="email" placeholder="smith.j@husky.neu.edu"
+                       value={this.state.email} onChange={this.handleChangeEmail}></input>
+            </div>
+              <div className="field">
+                <label>Password</label>
+                <input type="password" name="password"
+                       value={this.state.password} onChange={this.handleChangePassword}></input>
+              </div>
+              <Button onClick={this.handleSubmit}>Login</Button>
+        </form>
+        </div>
+
       </div>
     )
   }
